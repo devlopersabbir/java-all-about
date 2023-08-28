896d669dbc67dff368a8a00c9f856485d8106161
devlopersabbir <devlopersabbir@gmail.com>
2023-08-28 15:41:10 +0600

    Java Variable

--- Variable.java
+++ Variable.java
@@ -5,5 +5,15 @@ public class Variable {
          */
         String friendName = "Rafiul"; // this is varibale to store friend name
         System.out.println(friendName);
+
+        final int myNumber = 20;
+        System.out.println(myNumber);
+
+        // perform mathmetical task
+        int fNumber = 20;
+        int lNumber = 10;
+        // make a varibale for storing result inside the result variable
+        int result = fNumber + lNumber; // answer should be 30
+        System.out.println(result);
     }
 }
