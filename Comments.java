6764a07e4300de281f97e66279d94f5777aad52b
devlopersabbir <devlopersabbir@gmail.com>
2023-08-28 11:11:52 +0600

    Comments in Java

---
+++ Comments.java
@@ -0,0 +1,13 @@
+public class Comments {
+    // To returning my name
+    public static String name() {
+        return "Sabbir Hossain"; // it's return name as string
+    }
+
+    /*
+     * For showing my name
+     */
+    public static void main(String[] arg) {
+        System.out.println(name());
+    }
+}
