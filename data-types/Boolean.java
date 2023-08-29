9205c06c2dbd3c2912149ddf9851594b7621e2af
devlopersabbir <devlopersabbir@gmail.com>
2023-08-29 12:55:56 +0600

    Boolean Data Type

---
+++ data-types/Boolean.java
@@ -0,0 +1,15 @@
+public class Boolean {
+    public static void main(String[] args) {
+        /*
+         * Boolean Data type
+         * 
+         * Boolean type can only use 2 type of value like: true | false
+         * Yes | No
+         * On | Off
+         */
+        boolean isJavaFun = true;
+        boolean isNotFun = false;
+        System.out.println("Is Java Fun: " + isJavaFun);
+        System.out.println("Is not Fun: " + isNotFun);
+    }
+}
