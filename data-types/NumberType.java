8e0a9c4316074ce33f4d632c257ea0f57ea1aa65
devlopersabbir <devlopersabbir@gmail.com>
2023-08-29 12:34:27 +0600

    Number Data type

--- data-types/NumberType.java
+++ data-types/NumberType.java
@@ -1,6 +1,11 @@
 
 public class NumberType {
     public static void main(String[] args) {
-        System.out.println("hello");
+        byte a = 111; // bye not able to store more then 3 number
+        System.out.println("Byte: " + a);
+        short b = 5555; // short not able store more then 4 number
+        System.out.println("Short: " + b);
+        int c = 666666666; // int can store more then 9 numbers
+        System.out.println("Int: " + c);
     }
 }
