160cd0bad1953d0dd11fa6fa9af95b10cac2d35a
devlopersabbir <devlopersabbir@gmail.com>
2023-09-02 03:32:41 +0600

    Java do/while loop

---
+++ java-loops/JavaDoWhile.java
@@ -0,0 +1,9 @@
+public class JavaDoWhile {
+    public static void main(String[] arg) {
+        int i = 0;
+        do {
+            System.out.println("Hello " + i);
+            i++;
+        } while (i < 10);
+    }
+}
