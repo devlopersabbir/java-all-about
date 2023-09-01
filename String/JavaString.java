e7b87874f66d6aaaf935aafa4aff4220cc4c02ce
devlopersabbir <devlopersabbir@gmail.com>
2023-09-01 23:56:51 +0600

    String In Java

--- String/JavaString.java
+++ String/JavaString.java
@@ -16,5 +16,9 @@ public class JavaString {
         System.out.println(lowerCaseToUpper.toUpperCase());
         System.out.println(lowerCaseToUpper.toUpperCase());
 
+        String findAge = "My age is 20"; // Output: 10
+        System.out.println(findAge.indexOf("20"));// it is start counting from 0 since 20 belong into the 10 number
+                                                  // index then output is 10
+
     }
 }
