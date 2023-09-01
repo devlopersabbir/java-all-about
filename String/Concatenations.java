80d5a336bfdc0fe3d363f4fa9701e87d7f0a4d8a
devlopersabbir <devlopersabbir@gmail.com>
2023-09-02 00:01:30 +0600

    fixed simple issue

--- String/Concatenations.java
+++ String/Concatenations.java
@@ -1,6 +1,8 @@
 public class Concatenations {
     public static void main(String[] args) {
+        // simple way to concate
         String name = "Sabbir";
-        System.out.println("Name is: " + name);
+        System.out.println("Name is: " + name); // Name is: Sabbir
+
     }
 }
