5cce2586d47583c0ed9197a23bc8a025a0d00c23
devlopersabbir <devlopersabbir@gmail.com>
2023-09-02 02:54:27 +0600

    if condition

---
+++ if_else/IfCondition.java
@@ -0,0 +1,23 @@
+
+public class IfCondition {
+    /*
+     * Java Condition and if statement
+     */
+    public static void main(String[] args) {
+        // if statement
+        // Static value
+        if (20 < 30) {
+            System.out.println("true :(");
+        }
+        /*
+         * if else with dynamic
+         */
+        int a = 2;
+        int b = 30;
+        if (a < b) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+}
