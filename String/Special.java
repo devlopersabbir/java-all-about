cad9d39794f5a9e071014d074303c54f6d4e8a45
devlopersabbir <devlopersabbir@gmail.com>
2023-09-02 00:20:35 +0600

    Java Special

---
+++ String/Special.java
@@ -0,0 +1,19 @@
+public class Special {
+    public static void main(String[] agrs) {
+        String description = "Hi i'm sabbir. From \"Bangladesh\"."; // \" put it into double quote
+        System.out.println(description); // output: Hi i'm sabbir. From "Bangladesh"
+
+        String about = "HI, there, i\'m Sabbir."; // HI, there, i'm Sabbir.
+        System.out.println(about);
+
+        String details = "Me \\ you"; // Me \ you
+        System.out.println(details);
+
+        String txt = "Hello\nWorld!";
+        /*
+         * Hello
+         * World
+         */
+        System.out.println(txt);
+    }
+}
