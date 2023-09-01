658844b5f5a43fc0f7a499ff728ed69b0d5f0afa
devlopersabbir <devlopersabbir@gmail.com>
2023-09-02 02:27:50 +0600

    Bolean in java int

---
+++ Booleans.java
@@ -0,0 +1,7 @@
+public class Booleans {
+    public static void main(String[] args) {
+        boolean isJavaSimple = true; // true | false
+        System.out.println(isJavaSimple); // true
+
+    }
+}
