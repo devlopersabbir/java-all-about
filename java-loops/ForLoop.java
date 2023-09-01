1a136dd50d81cff33e6142b858099b2d8705152f
devlopersabbir <devlopersabbir@gmail.com>
2023-09-02 03:37:19 +0600

    Java For Loop

--- java-loops/ForLoop.java
+++ java-loops/ForLoop.java
@@ -1,5 +1,15 @@
 public class ForLoop {
     public static void main(String[] args) {
+        /*
+         * For Loop
+         */
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Number: " + i);
+        }
 
+        // everytime increment will be 2 times
+        for (int i = 0; i < 10; i = i + 2) {
+            System.out.println("Number2: " + i);
+        }
     }
 }
