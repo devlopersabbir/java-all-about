f58867238a51ade5463e10b345c91d8351435f16
devlopersabbir <devlopersabbir@gmail.com>
2023-09-02 00:11:34 +0600

    Java Num and String

--- String/Concatenations.java
+++ String/Concatenations.java
@@ -4,5 +4,7 @@ public class Concatenations {
         String name = "Sabbir";
         System.out.println("Name is: " + name); // Name is: Sabbir
 
+        String age = "19";
+        System.out.println("Age is: ".concat(age)); // concat() to contact 2 string
     }
 }
---
+++ String/Num_String.java
@@ -0,0 +1,7 @@
+public class Num_String {
+    public static void main(String[] args) {
+        int a = 10;
+        String b = "20";
+        System.out.println(a + b); // output: 1020
+    }
+}
