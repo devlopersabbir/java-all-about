4340d8314962e3bb0ba0ea5d92dc66d0cffc8cb7
devlopersabbir <devlopersabbir@gmail.com>
2023-09-02 13:03:11 +0600

    Java Method Meprameter

---
+++ method-ep-27/MethodParameter.java
@@ -0,0 +1,33 @@
+public class MethodParameter {
+    // Parameter is: name, age, roll
+    static void showDetails(String name, int age, int roll) {
+        System.out.println("Name: " + name + "\nAge: " + age + "\nRoll: " + roll);
+    }
+
+    /*
+     * store output into another variable
+     */
+    // Parameter is: name, age, roll
+    static String showHistory(String name, int age, int roll) {
+        return "Name: " + name + "\nAge: " + age + "\nRoll: " + roll;
+    }
+
+    static void checkAge(int age) {
+        if (age < 18) {
+            System.out.println("Access denied. Your are not old enogh!");
+        } else {
+            System.out.println("Access granted . Your are old enogh!");
+        }
+    }
+
+    public static void main(String[] args) {
+        // Argument passing
+        // showDetails("Sabbir Hossain Shuvo", 19, 652841);
+
+        // store output into another variable
+        // String result = showHistory("Sabbir Hossain Shuvo", 19, 652841);
+        // System.out.println(result);
+        checkAge(18);
+    }
+
+}
