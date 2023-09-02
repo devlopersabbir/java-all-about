2209571bfd04c5c1c196cb8845d5ca53545db655
devlopersabbir <devlopersabbir@gmail.com>
2023-09-03 03:02:34 +0600

    class and object

--- oop-ep-33/JavaClassObject.java
+++ oop-ep-33/JavaClassObject.java
@@ -1,3 +1,10 @@
 public class JavaClassObject {
+    // atribute
+    int x = 20;
 
+    public static void main(String[] args) {
+        // object
+        JavaClassObject myobj = new JavaClassObject();
+        System.out.println(myobj.x);
+    }
 }
