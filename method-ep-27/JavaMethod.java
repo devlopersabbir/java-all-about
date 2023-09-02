386ca95b9458d6eafb6380b0860782175ce1f105
devlopersabbir <devlopersabbir@gmail.com>
2023-09-02 12:43:40 +0600

    Java Method

---
+++ method-ep-27/JavaMethod.java
@@ -0,0 +1,14 @@
+public class JavaMethod {
+    // method creation
+    static void myMethod() {
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+        // method calling
+        myMethod();
+        myMethod();
+        myMethod();
+    }
+}
