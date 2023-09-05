90e7b8fbb7ec6df7db220f659f29da6af49bcf8b
devlopersabbir <devlopersabbir@gmail.com>
2023-09-05 21:30:57 +0600

    Java Modifire Final

--- oop-ep-33/java-modifiers/JavaModifires.java
+++ oop-ep-33/java-modifiers/JavaModifires.java
@@ -1,4 +1,17 @@
 
 public class JavaModifires {
+    final String myName = "Sabbir Hossain";
 
+    /**
+     * We can't change the my name varibale or override
+     *
+     * If we really want to do this then we will get this error!
+     * will generate an error: cannot assign a value to a final variable
+     */
+
+    public static void main(String[] args) {
+        JavaModifires getName = new JavaModifires();
+
+        System.out.println(getName.myName);
+    }
 }
