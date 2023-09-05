219fe0d6d1fcda9a0a77ec951fc792863f759370
devlopersabbir <devlopersabbir@gmail.com>
2023-09-05 21:46:05 +0600

    Public & Static Method

---
+++ oop-ep-33/java-modifiers/JavaStatic.java
@@ -0,0 +1,24 @@
+/**
+ * Different between public and static
+ */
+
+public class JavaStatic {
+    // Static method
+    static void getStaticMethod() {
+        System.out.println("Static method!");
+    }
+
+    // public method
+    public void getPublicMethod() {
+        System.out.println("Public method");
+    }
+
+    // main method
+    public static void main(String[] args) {
+        JavaStatic getMethod = new JavaStatic();
+
+        getStaticMethod(); // static method calling
+        getMethod.getStaticMethod(); // static method || it's not good way
+        getMethod.getPublicMethod(); // public method
+    }
+}
