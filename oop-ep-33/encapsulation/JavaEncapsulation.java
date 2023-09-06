486794050aa24201841a4968abdc00669a908f14
devlopersabbir <devlopersabbir@gmail.com>
2023-09-06 22:16:16 +0600

    Encapsulation Setter and getter

--- oop-ep-33/encapsulation/JavaEncapsulation.java
+++ oop-ep-33/encapsulation/JavaEncapsulation.java
@@ -1,4 +1,22 @@
 
 public class JavaEncapsulation {
+    public static void main(String[] args) {
+        Person personObj = new Person();
+        personObj.setName("Sabbir Hossain");
+        System.out.println(personObj.getName());
+    }
+}
 
+class Person {
+    private String name; // private = restricted access
+
+    // getter
+    public String getName() {
+        return name;
+    }
+
+    // setter
+    public void setName(String newName) {
+        this.name = newName;
+    }
 }
