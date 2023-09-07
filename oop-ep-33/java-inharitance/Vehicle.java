a91b68cec1d8dabeb8b255ac610ea5aa15fb7075
devlopersabbir <devlopersabbir@gmail.com>
2023-09-08 00:42:41 +0600

    Inharitance init

---
+++ oop-ep-33/java-inharitance/Vehicle.java
@@ -0,0 +1,16 @@
+class Vehicle {
+    protected String brand = "Ford";
+
+    public void honk() {
+        System.out.println("Tuut,,, tuut!");
+    }
+}
+
+class Car extends Vehicle {
+    private String modelName = "Mustag";
+
+    public static void main(String[] args) {
+        Car carObj = new Car();
+        System.out.println(carObj.brand);
+    }
+}
