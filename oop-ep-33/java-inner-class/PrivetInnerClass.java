48f5bd6c65984fc73c60e423f050f170364b80bf
devlopersabbir <devlopersabbir@gmail.com>
2023-09-10 19:53:29 +0600

    Java Inner Privet | Protected Class

--- oop-ep-33/java-inner-class/PrivetInnerClass.java
+++ oop-ep-33/java-inner-class/PrivetInnerClass.java
@@ -1,3 +1,19 @@
-public class PrivetInnerClass {
+class PrivetInnerClass {
+    public static void main(String[] args) {
+        OutterClass outer = new OutterClass();
 
+        System.out.println(outer.nam);
+
+        // OutterClass.InnerPrivetClass innerCls = outer.new InnerPrivetClass(); // this
+        // throw error because this a
+        // protected class
+    }
+}
+
+class OutterClass {
+    String nam = "Sabbir";
+
+    // private class InnerPrivetClass {
+    // int age = 19;
+    // }
 }
