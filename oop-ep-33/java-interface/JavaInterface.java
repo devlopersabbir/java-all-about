e856a91e82e33d34f6542920ddaa7c9300390974
devlopersabbir <devlopersabbir@gmail.com>
2023-09-11 22:20:37 +0600

    Java Interface

--- oop-ep-33/java-interface/JavaInterface.java
+++ oop-ep-33/java-interface/JavaInterface.java
@@ -1,3 +1,28 @@
-class JavaInterface {
+public class JavaInterface {
+    public static void main(String[] args) {
+        Ping obj = new Ping();
+        obj.animalSound();
+        obj.run();
+    }
+}
 
+class Ping implements Animal {
+    public void animalSound() {
+        System.out.println("pok pok");
+    }
+
+    public void run() {
+        System.out.println("go....");
+    }
+
+    public void again() {
+        System.out.println("again");
+    }
+}
+
+// interface
+interface Animal {
+    public void animalSound(); // // interface method (does not have a body)
+
+    public void run();
 }
