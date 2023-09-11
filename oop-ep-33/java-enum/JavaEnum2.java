e384dc6cb6a19d5e941c15a898e9d5a48892766e
devlopersabbir <devlopersabbir@gmail.com>
2023-09-11 22:51:27 +0600

    Java Enum example 2

---
+++ oop-ep-33/java-enum/JavaEnum2.java
@@ -0,0 +1,30 @@
+public class JavaEnum2 {
+    public static void main(String[] args) {
+        // for(String role: )
+        ERole role = ERole.USER;
+        switch (role) {
+            case ADMIN:
+                System.out.println("Admin");
+                break;
+            case EMPLOYEE:
+                System.out.println("Employee");
+                break;
+            case USER:
+                System.out.println("User");
+                break;
+            case SYSTEM_ADMIN:
+                System.out.println("System admin");
+                break;
+            default:
+                System.out.println("Simple person");
+                break;
+        }
+    }
+}
+
+enum ERole {
+    SYSTEM_ADMIN,
+    ADMIN,
+    USER,
+    EMPLOYEE
+}
