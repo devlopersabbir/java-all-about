49d3b185f816bab78ba275ba60334a3b6b333905
devlopersabbir <devlopersabbir@gmail.com>
2023-09-17 03:08:30 +0600

    Java input

--- oop-ep-33/java-user-input/JavaInput.java
+++ oop-ep-33/java-user-input/JavaInput.java
@@ -3,15 +3,25 @@ import java.util.Scanner;
 public class JavaInput {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        terminalMessage("start", true);
+        System.out.print("Enter your full name: ");
+        String fullName = scanner.nextLine();
         System.out.print("Enter your username: ");
-
         String username = scanner.nextLine();
-        if (username.length() > 1) {
-            System.out.println(username);
-        } else {
-            System.out.println("Input filed was empty!");
-        }
+        System.out.print("Enter your email: ");
+        String email = scanner.nextLine();
+
+        System.out.format("\nHello %s!\n", fullName);
 
         scanner.close();
+        System.out.println("");
+    }
+
+    static void terminalMessage(String message, boolean isMess) {
+        if (isMess) {
+            System.out.format("======Program %s======", message);
+        } else {
+            System.out.println(message);
+        }
     }
 }
