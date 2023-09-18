210dea59a8f4893b934b253d6a5faa40cb37aa54
devlopersabbir <devlopersabbir@gmail.com>
2023-09-19 01:52:16 +0600

    Java integer sorting

--- oop-ep-33/array_list/JavaSortNumeric.java
+++ oop-ep-33/array_list/JavaSortNumeric.java
@@ -4,6 +4,11 @@ import java.util.Collections;
 public class JavaSortNumeric {
     public static void main(String[] args) {
         ArrayList<Integer> roles = new ArrayList<Integer>();
+        roles.add(652841);
+        roles.add(652810);
+        roles.add(652838);
+        roles.add(652817);
+
         Collections.sort(roles);
         for (int role : roles) {
             System.out.println(role);
