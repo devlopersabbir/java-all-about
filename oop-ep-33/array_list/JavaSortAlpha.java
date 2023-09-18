d1b32a4516ef6488954ec7e1787c3998918a72c4
devlopersabbir <devlopersabbir@gmail.com>
2023-09-19 01:43:36 +0600

    Sort an ArrayList

---
+++ oop-ep-33/array_list/JavaSortAlpha.java
@@ -0,0 +1,19 @@
+import java.util.Collections;
+import java.util.ArrayList;
+
+public class JavaSortAlpha {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<String>();
+        names.add("Sabbir");
+        names.add("Tanvir");
+        names.add("Rafiul");
+        names.add("Tahisn");
+        names.add("Ankon");
+        names.add("Rizvi");
+
+        Collections.sort(names);
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
