0abd151ebabfb79a22cbef1b63df93a0a13462f6
devlopersabbir <devlopersabbir@gmail.com>
2023-09-19 01:12:27 +0600

    Array List init with String

---
+++ oop-ep-33/array_list/JavaArrayList.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList; // import java array list
+
+public class JavaArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Sabbir Cars");
+        cars.add("Rafi Cars");
+        cars.add("Tahsin Cars");
+        cars.add("Tanvir Cars");
+        System.out.println(cars);
+    }
+}
